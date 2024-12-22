0e25298324e9bbd8674886e04ffd82c9619daa27
Kashif0955 <154120701+Raheemullah8@users.noreply.github.com>
2024-12-22 01:57:45 -0800

    adding proper crud opration using ejs and mongodb

---
+++ part-6-mongodb-crud-with-ejs/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const userSchema = require('./model/user')
+
+app.set('view engine','ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.get('/',(req,res)=>{
+  res.render('index')
+});
+app.get('/read', async (req,res)=>{
+  let user = await userSchema.find()
+  res.render("read",{user} )
+});
+
+app.post('/create', async (req,res)=>{
+  try {
+    let {name,email,image} = req.body;
+    if(!name || !email || !image){
+      return res.status(400).json({error:"All Feaild are required"});
+    }
+    let createUser = await userSchema.create({
+      name,
+      email,
+      image
+    });
+    res.redirect('/read')
+  } catch (error) {
+    console.error('Error creating user:', error);
+    res.status(500).json({ error: 'Server error. Please try again later.' });    
+  }
+
+})
+app.post('/delete/:id',async (req,res)=>{
+  const user = await userSchema.findOneAndDelete({_id:req.params.id});
+  res.redirect('/read') ;
+})
+
+app.listen(3000);
