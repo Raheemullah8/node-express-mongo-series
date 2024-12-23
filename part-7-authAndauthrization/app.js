9cb48da2f47a6a132a8b73cc0c7a78862b89a51f
Kashif0955 <154120701+Raheemullah8@users.noreply.github.com>
2024-12-23 11:18:02 -0800

    Added functionality to store and verify JWT token in cookies. Now the JWT token can be read and verified using cookies.

--- part-7-authAndauthrization/app.js
+++ part-7-authAndauthrization/app.js
@@ -1,7 +1,29 @@
+// JWT token ko cookie mein store karne aur verify karne ka functionality add kiya gaya hai.
+//  Ab cookie ke zariye JWT token ko read aur verify kiya ja sakta hai.
+
 const express = require('express');
+const jwt = require('jsonwebtoken');
 const app = express();
-app.get('/',(req,res)=>{
-    res.cookie('name','raheem')
-    res.send('indexpage')
+const cookieParser = require('cookie-parser')
+
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+   let token = jwt.sign({email:"raheem@gmail.com"},"secret");
+   console.log("token=>",token)
+   res.cookie("jwttoken",token)
+    res.send("home")
 });
-app.listen(3000)
+
+app.get('/read', (req, res) => {
+    let data = jwt.verify(req.cookies.jwttoken,"secret");
+    console.log('data =>',data)
+    res.send('read cookie');
+});
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+
